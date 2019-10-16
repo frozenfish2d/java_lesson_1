ed61036b7c0f74cee4875bbf0493addd42d85c71
Sergei <43018199+frozenfish2d@users.noreply.github.com>
2019-10-16 23:28:02 +0300

    task 1 added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int array[] = {1, 2, 4, 2, 1, 4, 6, 4, 2, 1};
+        //Task 1
         int arrayRandom[] = new int[100];
-
+        for (int i = 0; i < arrayRandom.length; i++) {
+            arrayRandom[i] = (int) (Math.random() * 100);
+            System.out.print(arrayRandom[i] + " ");
+        }
+        System.out.println();
+        int max = arrayRandom[0];
+        int min = arrayRandom[0];
+        int avarage = 0;
+        for (int i = 0; i < arrayRandom.length; i++) {
+            if (max < arrayRandom[i]) {
+                max = arrayRandom[i];
+            }
+            if (min > arrayRandom[i]) {
+                min = arrayRandom[i];
+            }
+            avarage += arrayRandom[i] / arrayRandom.length;
+        }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+        System.out.println("avg = " + avarage);
     }
 }
