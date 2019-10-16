12b117557acb5823b5dc9bcb7082b03a1604d3bd
Sergei <43018199+frozenfish2d@users.noreply.github.com>
2019-10-16 23:08:25 +0300

    first load

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int array[] = {1, 2, 4, 2, 1, 4, 6, 4, 2, 1};
+        int arrayRandom[] = new int[100];
+
+    }
+}
